fb15d3ae11f791996f31947fbdfb6d527d7bc723
chowankumar <chowanmalhani5@gmail.com>
2024-05-19 14:32:34 +0500

    create folders for pages Home Login Profile Register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    
-    </>
-  )
-}
-
-export default App
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     <div>App</div>
+   )
+ }
+ 
+ export default App
