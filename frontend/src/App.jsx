66cc9925e57e5c6e1b9cb6d8a53eac9d0f8bd3d6
chowankumar <chowanmalhani5@gmail.com>
2024-05-21 02:43:47 +0500

    set the function which check if the user it not login then it will go to the login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,69 @@
 import React from 'react';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home.jsx';
 import Profile from './pages/Profile.jsx';
 import Rightbar from './components/Rigthbar.jsx'
 import Leftbar from './components/Leftbar.jsx';
 
-const Layout = () => {
-  return (
-    <div>
-      <Navbar />
-      <div className='flex'>
-        <Leftbar />
-        <div className='flex flex-6'>
-          <Outlet />
-        </div>
-        <Rightbar />
-      </div>
-    </div>
-  );
-};
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/profile/:id",
-        element: <Profile />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-]);
+
 
 const App = () => {
+
+  const currentUser = true
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div className='flex'>
+          <Leftbar />
+          <div className='flex flex-6'>
+            <Outlet />
+          </div>
+          <Rightbar />
+        </div>
+      </div>
+    );
+  };
+  
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to='/login' />
+    }
+    return children
+  }
+  
+  const router = createBrowserRouter([
+    {  
+      path: "/",
+      element:
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
   return (
     <div>
       <RouterProvider router={router} />
