fc5b7dd063d6fe7b0c4aec36f7e490a66c5361af
chowankumar <chowanmalhani5@gmail.com>
2024-05-20 13:24:21 +0500

    debug the  righBar  when we on the localhost/home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,59 @@
-import React from 'react'
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
-import { Route, Routes } from 'react-router-dom'
- 
- 
- const App = () => {
-   return (
-     <div>
-    <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register/>} />
-         
+import React from 'react';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import Home from './pages/Home.jsx';
+import Profile from './pages/Profile.jsx';
+import Rightbar from './components/Rigthbar.jsx'
+import Leftbar from './components/Leftbar.jsx';
 
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className='flex'>
+        <Leftbar />
+        <div className='flex flex-6'>
+          <Outlet />
+        </div>
+        <Rightbar />
+      </div>
+    </div>
+  );
+};
 
-      </Routes>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profile/:id",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 
-     </div>
-   )
- }
- 
- export default App
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
