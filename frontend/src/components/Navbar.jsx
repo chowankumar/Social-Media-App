fe5aae2037502ee41727e69bbb004a162f8a611d
chowankumar <chowanmalhani5@gmail.com>
2024-05-21 04:13:12 +0500

    ui of the navbar done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from 'react-router-dom';
+import DarkModeOutlined from '@mui/icons-material/DarkModeOutlined';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+  <div className="navbar flex justify-between items-center p-4 border-b shadow-md sticky">
+    <div className="left flex gap-8 items-center">
+      <Link to='/'>
+       <span className='font-bold text-[25px] text-[#00008b]' >Lamasocial</span>
+       </Link>
+       <HomeOutlinedIcon/>
+       <DarkModeOutlinedIcon/>
+       <GridViewOutlinedIcon/>
+       <div className="search border border-gray-300  rounded flex items-center gap-2 bg-transparent">
+        <SearchOutlinedIcon/>
+        <input className='w-[500px]' type="text" placeholder='Search...' />
+       </div>
+    </div>
+    <div className="right flex gap-6 items-center">
+      <PersonOutlinedIcon/>
+      <EmailOutlinedIcon/>
+      <NotificationsOutlinedIcon/>
+      <div className="user flex items-center">
+        <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png" alt="" className='w-12' />
+        <span className='text-[20px] font-semibold'>John Doe</span>
+
+      </div>
+    </div>
+  </div>
   )
 }
 
