6fb6620cfeb2a483cddfce737cbace0ba0d56a4d
chowankumar <chowanmalhani5@gmail.com>
2024-05-24 13:24:28 +0500

    set routing

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
 import Express from "express"
+import userRoutes from './routes/users.js'
+import authRoutes from './routes/auths.js'
+import postRoutes from './routes/posts.js'
+import commentRoutes from './routes/comments.js'
+import likeRoutes from './routes/likes.js'
+
 
 const app = Express()
+
+app.use("/app/auth",authRoutes)
+app.use("/app/users",userRoutes)
+app.use("/app/posts",postRoutes)
+app.use("/app/comments",commentRoutes)
+app.use("/app/likes",likeRoutes)
 
 app.listen(8000,()=>{
     console.log("running aplication at PORT 8000")
---
+++ backend/routes/posts.js
@@ -0,0 +1,8 @@
+import express from "express"
+import {} from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.get("",)
+
+export default router
