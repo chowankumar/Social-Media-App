1e74c385eb9711e6c6956dfdb87e777b11d804af
chowankumar <chowanmalhani5@gmail.com>
2024-05-31 02:41:17 +0500

    add multer

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,23 @@ app.use(
       origin: "http://localhost:5173",
     })
   );
+
+
+
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+
+
+const upload = multer({ storage: storage })
+  
   
 app.use(cookieParser())
 
