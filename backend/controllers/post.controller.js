d8e9e3fb07272e8ea74f123ac2d8e1674297dea2
chowankumar <chowanmalhani5@gmail.com>
2024-05-31 02:37:31 +0500

    add post method

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,5 +1,7 @@
+import moment from "moment/moment.js";
 import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
+
 
 export const getPosts = (req, res) => {
   const token = req.cookies.accessToken;
@@ -23,6 +25,9 @@ export const getPosts = (req, res) => {
   });
 };
 
+
+
+
 export const addPost = (req, res) => {
     const token = req.cookies.accessToken;
     if (!token) return res.status(401).json("Not logged in!");
@@ -30,17 +35,18 @@ export const addPost = (req, res) => {
     jwt.verify(token, "secretkey", (err, userInfo) => {
       if (err) return res.status(403).json("Token is not valid");
   
-      const q = `
-        SELECT p.*, u.id AS userId, u.name, u.profilePic 
-        FROM posts AS p 
-        JOIN users AS u ON u.id = p.userId 
-        LEFT JOIN relation AS r ON r.followedUserId = p.userId 
-        WHERE r.followerUserId = ? OR p.userId = ?
-      `;
+      const q = "INSERT INTO posts (`desc`, `img`, `createdAt`, `userId`) VALUES (?, ?, ?, ?)";
   
-      db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+      const values = [
+        req.body.desc,
+        req.body.img,
+        moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+        userInfo.id,
+      ];
+  
+      db.query(q, values, (err, data) => {
         if (err) return res.status(500).json(err);
-        return res.status(200).json(data);
+        return res.status(200).json("Post has been created");
       });
     });
   };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
      <div>
       <Stroies/>
-      {/* <Share/> */}
+      <Share/> 
       <Posts/>
      </div>
   )
