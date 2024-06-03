a37faca0978d39caaf89f32585a54e62a6624584
chowankumar <chowanmalhani5@gmail.com>
2024-06-04 03:00:36 +0500

    ui of the navabr done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,25 +13,22 @@ const Navbar = () => {
     return (
         <div className="p-2.5 h-20 sticky top-0 z-[999] flex justify-between items-center bg-white border-b shadow-md">
             <div className="left flex gap-8 items-center">
-                <span className='font-bold text-[25px] text-[#00008b]'>Social-With-54</span>
+                <span className='font-bold text-[35px] text-[#0866ff]'>Social-with-54</span>
                 <Link to='/'>
-                    <HomeOutlinedIcon />
+                    <HomeOutlinedIcon className='text-[#0866ff] text-[100px]' />
                 </Link>
-                <div className="search border border-gray-300 rounded flex items-center gap-2 bg-transparent">
-                    <SearchOutlinedIcon />
-                    <input className='w-[500px]' type="text" placeholder='Search...' />
-                </div>
+                
             </div>
             <div className="right flex gap-6 items-center">
-                <PersonOutlinedIcon />
-                <EmailOutlinedIcon />
-                <NotificationsOutlinedIcon />
+                <PersonOutlinedIcon className='text-[#0866ff] text-[100px] '/>
+                <EmailOutlinedIcon className='text-[#0866ff] text-[100px]'/>
+                <NotificationsOutlinedIcon className='text-[#0866ff] text-[100px]'/>
                 <div className="user flex gap-2 items-center">
                     {currentUser && (
                         <>
                             <img src={currentUser.profilePic} alt="" className='w-12 rounded-full' />
                             <span className='text-[20px] font-semibold'>{currentUser.name}</span>
-                            <button onClick={logout} className='ml-4 px-4 py-2 bg-[#00008b] text-white rounded'>
+                            <button onClick={logout} className='ml-4 px-4 py-2 bg-[#0866ff] text-white rounded font-medium'>
                                 Logout
                             </button>
                         </>
